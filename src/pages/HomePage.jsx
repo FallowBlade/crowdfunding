e4191003bea7fe2d2377be42fd2c09028ed80cf7
FallowBlade <113985916+FallowBlade@users.noreply.github.com>
2023-02-18 09:41:58 +0800

    Save

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,10 +1,23 @@
 // Components
+
+import { useState, useEffect } from React;
+
 import ProjectCard from "../components/ProjectCard/ProjectCard";
 
 // Data
 import { allProjects } from "../data";
+import { Form } from "react-router-dom";
+import React from "react";
 
 function HomePage() {
+    const [projectList, setProjectList] = useState([]);
+
+    useEffect(() => {
+        setProjectList(allProjects);
+    }, []);
+
+
+
     return (
         <div id="project-list">
             {allProjects.map((project, key) => {
